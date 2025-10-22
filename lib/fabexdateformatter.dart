5621cf1a55161e3540ae9ef2b3833205c349947a
Fathah KA <48355244+fathah@users.noreply.github.com>
2025-10-22 22:10:20 +0400

    Update fabexdateformatter.dart

--- lib/fabexdateformatter.dart
+++ lib/fabexdateformatter.dart
@@ -3,21 +3,20 @@ library fabexdateformatter;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class FabexFormatter{
-
-/// You can call several useful methods through the FabexFormatter Class
+class FabexFormatter {
+  /// You can call several useful methods through the FabexFormatter Class
 
   String timeOfDayToJM(TimeOfDay tod) {
-    
     /// Convert TimeOfDay to time
     /// Example: ```TimeOfDay(22, 30)``` returns ```10:30 PM```
-    
+
     final now = new DateTime.now();
     final dt = DateTime(now.year, now.month, now.day, tod.hour, tod.minute);
-    final format = DateFormat.jm(); ///"6:00 AM"
+    final format = DateFormat.jm();
+
+    ///"6:00 AM"
     return format.format(dt);
   }
-
 
   String timeOfDayToDateTimeString(TimeOfDay tod) {
     /// Convert TimeOfDay to Date and return as a String
@@ -35,7 +34,8 @@ class FabexFormatter{
 
   TimeOfDay dateTimeToTimeOfDay(DateTime date) {
     /// Convert DateTime to TimeOfDay
-    final dt = DateTime(date.year, date.month, date.day, date.hour, date.minute);
+    final dt =
+        DateTime(date.year, date.month, date.day, date.hour, date.minute);
     return TimeOfDay.fromDateTime(dt);
   }
 
@@ -50,15 +50,12 @@ class FabexFormatter{
     return "$time - $resdDate";
   }
 
-
   DateTime firestoreTimeToDateTime(firebasDate) {
     /// Convert Firebase Firestore Time to flutter DateTime.
     /// Return as DateTime.
     /// Example: ```September 13 2020 at 6:36:56 PM``` returns ```DateTime(2020, 09, 13, 06, 36, 5600)```
     return DateTime.parse(firebasDate.toDate().toString());
-
   }
-
 
   List firestoreTimeToMonthYear(firebasDate) {
     /// Convert Firebase Firestore Time to flutter DateTime and returns month and year as list.
@@ -87,45 +84,46 @@ class FabexFormatter{
     String resdDate = DateFormat('dd/MM/yyyy').format(parsed);
     return resdDate;
   }
+
   String getMonthName(int monthNumber) {
     ///Get the name of the entered month
     switch (monthNumber) {
       case 1:
         return "January";
-        break;
+
       case 2:
         return "February";
-        break;
+
       case 3:
         return "March";
-        break;
+
       case 4:
         return "April";
-        break;
+
       case 5:
         return "May";
-        break;
+
       case 6:
         return "June";
-        break;
+
       case 7:
         return "July";
-        break;
+
       case 8:
         return "August";
-        break;
+
       case 9:
         return "September";
-        break;
+
       case 10:
         return "October";
-        break;
+
       case 11:
         return "November";
-        break;
+
       case 12:
         return "December";
-        break;
+
       default:
         return "Unknown";
     }
@@ -134,9 +132,9 @@ class FabexFormatter{
   String prevYear(currentYear) {
     // Get the Previous Year
     int curYr;
-    if(currentYear is String){
+    if (currentYear is String) {
       curYr = int.parse(currentYear);
-    }else{
+    } else {
       curYr = currentYear;
     }
     int finalYr = curYr - 1;
@@ -162,52 +160,51 @@ class FabexFormatter{
       return currentMonth - 1;
     }
   }
+
   String getCurrentMonthName() {
     ///Get the name of the current month
     DateTime now = DateTime.now();
     String month = DateFormat('MM').format(now);
-    int  monthNumber = int.parse(month);
+    int monthNumber = int.parse(month);
     switch (monthNumber) {
       case 1:
         return "January";
-        break;
+
       case 2:
         return "February";
-        break;
+
       case 3:
         return "March";
-        break;
+
       case 4:
         return "April";
-        break;
+
       case 5:
         return "May";
-        break;
+
       case 6:
         return "June";
-        break;
+
       case 7:
         return "July";
-        break;
+
       case 8:
         return "August";
-        break;
+
       case 9:
         return "September";
-        break;
+
       case 10:
         return "October";
-        break;
+
       case 11:
         return "November";
-        break;
+
       case 12:
         return "December";
-        break;
+
       default:
         return "Unknown";
     }
   }
-
 }
-
